ac24be7f5a2c8fddd329b612d5a2d00b15cc5840
csbsantos <claudiasbsantos13@gmail.com>
2025-07-11 11:28:18 +0100

    Update Main.java

--- src/Lecture/Main.java
+++ src/Lecture/Main.java
@@ -1,24 +1,14 @@
-package Blackjack;
+import Blackjack.Game;
+import Blackjack.Player;
 
 public class Main {
     public static void main(String[] args) {
-        Student s1= new Student("Cláudia", 1);
-        Student s2= new Student("Rui", 2);
-        Student s3= new Student("João", 3);
+       Game g1= new Game();
+        Player p1;
+        Player p2;
 
-        Teacher t1= new Teacher("Joaquim Almeida");
-        Teacher t2= new Teacher("Carlos Peixoto");
 
-        Lecture l1= new Lecture("Java");
-
-        l1.addNewStudent(s1);
-        l1.addNewStudent(s2);
-        l1.addNewStudent(s3);
-
-//   l1.addNewStudent(new Student []{s1,s2,s3});
-//
-//   Student [] bunchOfStudents= new Student[]{s1, s2, s3};
-//   l1.addNewStudent(bunchOfStudents);
-l1.printAllStudents();
+        g1.start();
+        g1.determineWinner();
     }
 }
